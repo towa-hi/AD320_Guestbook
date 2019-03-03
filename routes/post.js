ac62d2311b92fab854b4273b359b183645d7985d
Dereje Desta <ddereje777@gmail.com>
2019-03-02 17:13:03 -0800

    help needed!

--- routes/post.js
+++ routes/post.js
@@ -1,1 +1,13 @@
 //server side code for handling posting goes here
+var app = require('../app');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+app.get('/preferences', function(req,res){
+	res.render('preferences', {qs: req.query});
+});
+
+app.post('/preferences',urlencodedParser, function(req,res){
+	console.log(req.body);
+	res.render('preferences', {qs: req.query});
+});
