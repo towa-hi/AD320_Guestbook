5cddb98beaafee006607c2cc9617af9883a4d906
UdonPC <cyber@8chan.co>
2019-03-02 18:28:42 -0800

    added pagination, added auto hiding of features from preferences like removing the name field if posting with names is disabled

--- public/script.js
+++ public/script.js
@@ -1,23 +1,75 @@
 //This runs when the page loads. right now it gets all the data from the api, generates a page and then inserts it into index.html #display div
+
+var currentPage = 0;
+var maxPages = 10;
+var lastPage = 0;
+var commentsPerPage = 6;
+var commentArray;
+
+var enablePostName = 1;
+var enablePostEmail = 1;
+var displayPostName = 1;
+var displayPostEmail = 1;
+var displayPostDate = 1;
+var defaultCommentsPerPage;
+var defaultMaxPages;
+
 $(document).ready(function(){
 	$.getJSON("http://localhost:3000/api/v1/comments/all", (result)=>{
-		var pageHTML = generatePage(result);
-		$("#display").append(pageHTML);
+		commentArray =  result;
+		lastPage = Math.round(commentArray.length / commentsPerPage) - 1;
+		drawPage();
 	});
+	hideFields();
 });
+
+function drawPage() {
+	var pageHTML = generatePage();
+	$("#display").html(pageHTML);
+	
+}
+
+function hideFields() {
+	if (enablePostName === 0) {
+		$('#input-container-name').hide();
+	}
+	if (enablePostEmail === 0) {
+		$('#input-container-email').hide();
+	}
+}
 //Function takes a database array and returns the page with the appropriate contents. pagination should go here.
-function generatePage(commentArray) {
+function generatePage() {
 	var pageHTML = "";
-	for (count = 0; count < commentArray.length; count++) {
-		var comment = commentArray[count];
-		var commentHTML = commentToHTML(comment);
-		pageHTML += commentHTML;
+	for (count = currentPage * commentsPerPage; count < (currentPage * commentsPerPage) + commentsPerPage; count++) {
+		if (count < commentArray.length){
+			var comment = commentArray[count];
+			var commentHTML = commentToHTML(comment);
+			pageHTML += commentHTML;
+		}
 	}
 	return pageHTML;
 }
 
 function commentToHTML(comment) {
-	var commentHTML = "<div class='message-container'><div id='title-container'><div id='name-linked'><a href='mailto:" + comment.Email + "'>" + comment.PostName + "</a></div><div id='date'>" + convertFromUnixTimestamp(comment.PostDate) + "</div></div><hr><div id='message-content'>" + comment.Message + "</div></div>";
+	var commentHTML = "<div class='message-container'><div id='title-container'>";
+	if (displayPostEmail == 1) {
+		commentHTML += "<div id='name-linked'><a href='mailto:" + comment.Email + "'>";
+	} else {
+		commentHTML += "<div id='name-linked'>";
+	}
+	if (displayPostName == 1) {
+		commentHTML += comment.PostName;
+	} else if (displayPostName == 0 || comment.PostName === null){
+		commentHTML += "Anonymous";
+	}
+	if (displayPostEmail == 1) {
+		commentHTML += "</a>";
+	}
+	commentHTML += "</div>";
+	if (displayPostDate == 1){
+		commentHTML += "<div id='date'>" + convertFromUnixTimestamp(comment.PostDate) + "</div>";
+	}
+	commentHTML += "</div><hr><div id='message-content'>" + comment.Message + "</div></div>"
 	return commentHTML;
 }
 
@@ -29,6 +81,24 @@ function convertFromUnixTimestamp(timestamp) {
 	var date = a.getDate();
 	var time = month + ' ' + date + ' ' + year;
 	return time;
+}
+
+function backPage() {
+	if (currentPage > 0) {
+		currentPage--;
+		drawPage();
+	} else {
+		console.log("can't execute backPage(). Already on first page!");
+	}
+}
+
+function nextPage() {
+	if (currentPage < lastPage) {
+		currentPage++;
+		drawPage();
+	} else {
+		console.log("can't execute nextPage(). Already on last page!");
+	}
 }
 
 //Kyle's char count function
--- routes/comments.js
+++ routes/comments.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+
 //if /all is called, return the full results of the db.query. to select what kind of info to return, limit the query.
+
+
 router.get('/all', function(req, res, next) {
-  db.query("select * from messages order by PostDate DESC", (error, result, fields) => {
-    if (error) {
-      res.status(500).send(error);
-    }
-    res.send(result);
-  });
+	db.query("select * from messages order by PostDate DESC", (error, result, fields) => {
+		if (error) {
+			res.status(500).send(error);
+		}
+		res.send(result);
+	});
 });
+
+
 
 module.exports = router;
