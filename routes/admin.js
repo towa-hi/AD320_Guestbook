d4165b199257ec474d7ffc69e837beff718749ef
Dereje Desta <ddereje777@gmail.com>
2019-03-10 15:06:25 -0700

    user stories 17,28,39

---
+++ routes/admin.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+
+router.post('/', function(req,res){
+
+ var pref = [
+    req.body.name_post,
+    req.body.email_post,
+    req.body.message_post,
+    req.body.date_post,
+    req.body.name_input,
+    req.body.email_input,
+    req.body.comment1,
+    req.body.comment2,
+
+    ];
+    for(var i =0; i<pref.length; i++){
+        if(pref[i]== null){
+            pref[i]=0;
+        }
+    }
+ var query =`UPDATE PREFERENCES 
+            SET PostName = ?, PostEmail = ?, PostMessage = ?, DisplayDate = ?, DisplayName = ?, 
+            DisplayEmail= ?, PageResults = ?, TotalResults = ? 
+            WHERE PreferenceID = 1`;
+   var queryParams = pref;
+	
+  db.query(query, queryParams, (error, result, fields) => {
+    if (error){
+      res.status(500).send(error);
+    }
+    res.status(201).send('preferences updated');
+  });
+  
+});
+
+
+module.exports = router;
+
+
+
