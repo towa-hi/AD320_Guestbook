9570f41a452f8c6781d2ea7c5764443d1b6ee6f7
Dereje Desta <ddereje777@gmail.com>
2019-03-16 01:37:34 -0700

    adminLogin added and preferences reflected in admin.html

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser=require('body-parser');
 
 var indexRouter = require('./routes/index');
 var commentsRouter = require('./routes/comments');
 var postRouter = require('./routes/post');
 var prefRouter = require('./routes/preferences');
-
+var adLoginRoute = require('./controllers/authenticate')
 var adminPref = require('./routes/admin');
+
 
 var delRouter = require('./routes/delete');
 
 //initialize app
 var app = express();
 //set paths
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -31,6 +35,7 @@ app.use('/api/v1/preferences', prefRouter);
 app.use('/api/v1/ad', adminPref);
 
 app.use('/api/v1/delete', delRouter);
+app.use('/api/v1/authenticate', adLoginRoute);
 
 
 module.exports = app;
---
+++ public/adminpage.js
@@ -0,0 +1,19 @@
+
+var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
+    $checkboxes = $("#admin-form2 :checkbox");
+
+$checkboxes.on("change", function(){
+  $checkboxes.each(function(){
+    checkboxValues[this.id] = this.checked;
+  });
+  
+  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+});
+
+// On page load
+$.each(checkboxValues, function(key, value) {
+  $("#" + key).prop('checked', value);
+});
+
+
+
